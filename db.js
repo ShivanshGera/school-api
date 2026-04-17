d4f5974d6b1ff0d02578531d6fcb4e9aae997910
ShivanshGera <shivanshgera15@gmail.com>
2026-04-18 02:13:02 +0530

    final fix for railway deployment

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
-const db = mysql.createConnection(process.env.MYSQL_URL);
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT)
+});
 
 db.connect((err) => {
   if (err) {
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 // import routes
 const schoolRoutes = require("./routes/schoolRoutes");
