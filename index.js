587f2161adbd4fbe2208bc4574ef42980c49536e
ShivanshGera <shivanshgera15@gmail.com>
2026-04-18 02:34:12 +0530

    final working config

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config(); 
 
-// import routes
 const schoolRoutes = require("./routes/schoolRoutes");
 
-// initialize app
 const app = express();
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 
-// routes
 app.use("/api", schoolRoutes);
 
-// basic route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-// server start
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
